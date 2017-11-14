f830e044d358cb8a2ba964d60ac72d871b696601
jensjoh01 <johnnyj2439@gmail.com>
2017-11-14 14:42:19 -0800

    able to updated database with heard true false

--- lambda_function.py
+++ lambda_function.py
@@ -1,19 +1,17 @@
+"""."""
+
 import boto3
 import datetime
-# from rx import Observable
+from rx import Observable
 
 
 def lambda_handler(event, context):
     """."""
-    # show_table() #remove this
-    # TODO implement
     if (event["session"]["application"]["applicationId"] !=
             "amzn1.ask.skill.ff117040-72fc-409a-a82f-cdba631d7f2d"):
         raise ValueError("Invalid Application ID")
-
     if event["session"]["new"]:
         on_session_started({"requestId": event["request"]["requestId"]}, event["session"])
-    # print(event)
     if event["request"]["type"] == "LaunchRequest":
         return on_launch(event["request"], event["session"])
     elif event["request"]["type"] == "IntentRequest":
@@ -22,34 +20,20 @@ def lambda_handler(event, context):
         return on_session_ended(event["request"], event["session"])
 
 
-# this function adds an entry in the database
-def show_table():
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-    table = dynamodb.Table('aim_messages')
-    table.put_item(Item={
-        'id': 4,
-        'receiver_name': 'Darren',
-        'date': '11/13/2017',
-        'message': 'Take off ya hoser!',
-        'sender_name': 'John'
-    })
-    response = table.scan()
-    print(sorted(response['Items'], key=lambda x: x['sender_name']))
-
-
 def on_session_started(session_started_request, session):
+    """Function to handle any initiation on new session."""
     print("Starting new session.")
 
 
 def on_launch(launch_request, session):
+    """."""
     return get_welcome_response()
-    # this function launches the appropriate intent based on utterance
 
 
 def on_intent(intent_request, session):
+    """."""
     intent = intent_request["intent"]
     intent_name = intent_request["intent"]["name"]
-
     if intent_name == "EstablishRecipient":
         return get_recipient(intent, session)
     elif intent_name == "ReceiveMessage":
@@ -62,12 +46,6 @@ def on_intent(intent_request, session):
         return on_session_ended()
     else:
         raise ValueError("Invalid intent")
-
-"""
-*****************************************************************************************************
-
-
-"""
 
 
 def get_recipient(intent, session):
@@ -83,6 +61,7 @@ def get_recipient(intent, session):
 
 
 def verification_of_message(intent, session):
+    """."""
     message_body = intent["slots"]["Message"]["value"]
     session["attributes"]["message_body"] = message_body
     card_title = "AIM"
@@ -97,6 +76,7 @@ def verification_of_message(intent, session):
 
 
 def save_msg_to_db(session):
+    """."""
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
     table = dynamodb.Table('aim_messages')
     db_response = table.scan()
@@ -107,29 +87,40 @@ def save_msg_to_db(session):
         'receiver_name': session["attributes"]["recipient"],
         'date': datetime.datetime.now().strftime('%m/%d/%y'),
         'message': session["attributes"]["message_body"],
-        'sender_name': "RedCoat"
+        'sender_name': "RedCoat",  # do we want to include sender name?
+        'heard': False
     })
 
 
 def receive_message(intent, session):
+    """Return the messages based on receiver name."""
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
     table = dynamodb.Table('aim_messages')
     db_response = table.scan()
     session_attributes = {}
-    sender_name = intent["slots"]["Name"]["value"].lower()
+    receiver_name = intent["slots"]["Name"]["value"].lower()
     message = []
     speech_output = ""
     for index, row in enumerate(db_response["Items"]):
-        if row['sender_name'].lower() == sender_name:
+        if row['receiver_name'].lower() == receiver_name and not row['heard']:
             message.append(row["message"])
+            table.update_item(
+                Key={
+                    "id": row["id"],
+                    "receiver_name": row["receiver_name"]
+                },
+                UpdateExpression="SET heard = :bool",
+                ExpressionAttributeValues={
+                    ":bool": "true"
+                })
     card_title = "AIM"
     number_of_messages = len(message)
     if not number_of_messages:
-        speech_output = "You don't have any messages from {}. ".format(sender_name)
+        speech_output = "There are no messages for {}. ".format(receiver_name)
     elif number_of_messages == 1:
-        speech_output = "This is your message from {}. {} ".format(sender_name, message[0])
+        speech_output = "Here is your message for {}. {} ".format(receiver_name, message[0])
     else:
-        speech_output = "Here are your messages from {}. ".format(sender_name)
+        speech_output = "Here are the messages for {}. ".format(receiver_name)
         for index, value in enumerate(message):
             speech_output += "Message {}. {}. ".format(index + 1, value)
     reprompt_text = ""
